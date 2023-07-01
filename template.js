42a2cc5ff835d07cbbb1f8afdc7cca90d45fd0da
hamitmizrak <hamitmizrak@gmail.com>
2023-07-01 16:00:25 +0300

    operator, Whoisting, naming convensition, Prompt

--- template.js
+++ template.js
@@ -23,10 +23,40 @@ console.log(typeof data+" => "+data);
 data="JavaScript Öğreniyorum";
 console.log(typeof data+" => "+data);
 
+// naming convensition
+var science="JS tutorials";
+var science44="JS tutorials";
+var _science44="JS tutorials";
+var $science44="JS tutorials";
+var scienceData="JS tutorials";
+
+//var 44scienceData="JS tutorials";
+//var ~scienceData="JS tutorials";
+
 // Whoisting 
 deneme=55;
 var deneme;
 console.log(typeof deneme+" => "+deneme);
 
+// Operator
+console.log(4+6);
+console.log(4-6);
+console.log(4*6);
+console.log(10/2);
+console.log(10%2);
+
+// Operator && || ! > >= < <=
+
+// =   ATAMA
+// ==  EŞİTTİR ama Türüne bakmadan
+// === EŞİTTİR ama Türüne bakacaksın
+
+// x++
 
 
+// Prompt
+var userData=prompt("Lütfen birşeyler yazınız");
+alert(userData);
+
+// Kullanıcıdan aldığımız 2 sayıyı toplayan JS kodu yazınız ?
+// Kullanıcıdan aldığımız sayıyı dereceden fahrenhyat'a çeviren JS kodu yazınız ?
