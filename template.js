9042339e5b57ec1ed03513b3a1814083f50df002
hamitmizrak <hamitmizrak@gmail.com>
2023-07-01 15:38:39 +0300

    JS

---
+++ template.js
@@ -0,0 +1,32 @@
+// console
+// console.log("console log");
+// console.error("console error");
+// console.warn("console warning");
+// console.info("console info");
+
+// document
+//document.writeln("document data");
+
+// Alert
+//alert("Alert Data");
+
+// variable , typeof
+var data = 44;
+console.log(typeof data+" => "+data);
+
+data=12.23;
+console.log(typeof data+" => "+data);
+
+data=true;
+console.log(typeof data+" => "+data);
+
+data="JavaScript Öğreniyorum";
+console.log(typeof data+" => "+data);
+
+// Whoisting 
+deneme=55;
+var deneme;
+console.log(typeof deneme+" => "+deneme);
+
+
+
