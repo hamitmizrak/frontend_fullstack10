6489ad5499596cdcd86e49131bbbba003123784d
hamitmizrak <hamitmizrak@gmail.com>
2023-07-01 17:21:33 +0300

    cast, let, const, undefined NaN isNaN infinity

--- template.js
+++ template.js
@@ -60,32 +60,70 @@
 
 // Örnek-1
 // Kullanıcıdan aldığımız 2 sayıyı toplayan JS kodu yazınız ?
-var data1 = Number(prompt("Lütfen 1.sayıyı giriniz"));
-var data2 = Number(prompt("Lütfen 2.sayıyı giriniz"));
-var result1 = ((data1 + data2));
-console.log(result1)
+// var data1 = Number(prompt("Lütfen 1.sayıyı giriniz"));
+// var data2 = Number(prompt("Lütfen 2.sayıyı giriniz"));
+// var result1 = ((data1 + data2));
+// console.log(result1)
 
 // Örnek-2
 /* 
 Kullanıcıdan aldığımız sayıyı dereceden fahrenhyat'a çeviren JS kodu yazınız ?
 Formül: (derece*9/5)+32 
 */
-var degree, fahranhayt;
-degree = Number(prompt("Lütfen derece Giriniz"));
-fahranhayt = (degree * 9 / 5) + 32;
-console.log(degree + " Derece " + fahranhayt + " fahranhayt")
+// var degree, fahranhayt;
+// degree = Number(prompt("Lütfen derece Giriniz"));
+// fahranhayt = (degree * 9 / 5) + 32;
+// console.log(degree + " Derece " + fahranhayt + " fahranhayt")
 
 // Örnek-3
 // y=3x+4k ==>1.dereceden 2 bilinmeyenli denklem algoritması
 // Kullanıcı tarafından alınan x ve kdeğerlerini hesaplayan algoritma yazınız ?
+// var x= Number(prompt("Lütfen x değerini Giriniz"));
+// var k= Number(prompt("Lütfen k değerini Giriniz"));
+// var result3=3*x+4*k;
+//console.log(result3)
+
 
 // örnek-4
 // operatör işlemleri: aşağıdaki örneği javascript ile yapalım
 // 4+3*2(3:3-1*6+9:1+(3:3));
+console.log(4+3*2*(3/3-1*6+9/1+(3/3)));
 
-
+/////////////////////////////////////////////////////////////////
 // cast
+// Number(prompt("Lütfen derece Giriniz"));
+// String(prompt("Lütfen derece Giriniz"));
+
+
+// var=Global, 
+// var sayi1=44;
+// var sayi1=44;
+
+// let= local variable , 
+// let sayi2=44;
+// sayi2=23;
+
+// const=sabit,local variable
+// const sayi3=99;
+// sayi3=23; // yazamazsınız
+// console.log(sayi3);
+
 // undefined NaN isNaN infinity
+//undefined // tanımsız bir değişkene atanmış degerdir
+let data4;
+console.log(data4);
+
+//NaN 
+console.log("kelime"/4)
+
+//infinity
+console.log(555/0);
+
+//null      // boş veri tipidir ve herhangi biri null olarak atandığında
+//isNaN()   // eksik veya hatalı girişler için kullanılır
+//Infinity: Sonsuzluk
+
+/////////////////////////////////////////////////////////////////
 // Math
 // Sayı
 // Kelime
