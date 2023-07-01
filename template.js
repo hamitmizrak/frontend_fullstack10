be819539e75283664e434ff44eb2f79a557417e0
hamitmizrak <hamitmizrak@gmail.com>
2023-07-01 16:33:59 +0300

    examples

--- template.js
+++ template.js
@@ -10,40 +10,40 @@
 // Alert
 //alert("Alert Data");
 
-// variable , typeof
-var data = 44;
-console.log(typeof data+" => "+data);
+// // variable , typeof
+// var data = 44;
+// console.log(typeof data+" => "+data);
 
-data=12.23;
-console.log(typeof data+" => "+data);
+// data=12.23;
+// console.log(typeof data+" => "+data);
 
-data=true;
-console.log(typeof data+" => "+data);
+// data=true;
+// console.log(typeof data+" => "+data);
 
-data="JavaScript Öğreniyorum";
-console.log(typeof data+" => "+data);
+// data="JavaScript Öğreniyorum";
+// console.log(typeof data+" => "+data);
 
-// naming convensition
-var science="JS tutorials";
-var science44="JS tutorials";
-var _science44="JS tutorials";
-var $science44="JS tutorials";
-var scienceData="JS tutorials";
+// // naming convensition
+// var science="JS tutorials";
+// var science44="JS tutorials";
+// var _science44="JS tutorials";
+// var $science44="JS tutorials";
+// var scienceData="JS tutorials";
 
-//var 44scienceData="JS tutorials";
-//var ~scienceData="JS tutorials";
+// //var 44scienceData="JS tutorials";
+// //var ~scienceData="JS tutorials";
 
-// Whoisting 
-deneme=55;
-var deneme;
-console.log(typeof deneme+" => "+deneme);
+// // Whoisting 
+// deneme=55;
+// var deneme;
+// console.log(typeof deneme+" => "+deneme);
 
-// Operator
-console.log(4+6);
-console.log(4-6);
-console.log(4*6);
-console.log(10/2);
-console.log(10%2);
+// // Operator
+// console.log(4+6);
+// console.log(4-6);
+// console.log(4*6);
+// console.log(10/2);
+// console.log(10%2);
 
 // Operator && || ! > >= < <=
 
@@ -55,8 +55,39 @@ console.log(10%2);
 
 
 // Prompt
-var userData=prompt("Lütfen birşeyler yazınız");
-alert(userData);
+// var userData=prompt("Lütfen birşeyler yazınız");
+// alert(userData);
 
+// Örnek-1
 // Kullanıcıdan aldığımız 2 sayıyı toplayan JS kodu yazınız ?
-// Kullanıcıdan aldığımız sayıyı dereceden fahrenhyat'a çeviren JS kodu yazınız ?
+var data1 = Number(prompt("Lütfen 1.sayıyı giriniz"));
+var data2 = Number(prompt("Lütfen 2.sayıyı giriniz"));
+var result1 = ((data1 + data2));
+console.log(result1)
+
+// Örnek-2
+/* 
+Kullanıcıdan aldığımız sayıyı dereceden fahrenhyat'a çeviren JS kodu yazınız ?
+Formül: (derece*9/5)+32 
+*/
+var degree, fahranhayt;
+degree = Number(prompt("Lütfen derece Giriniz"));
+fahranhayt = (degree * 9 / 5) + 32;
+console.log(degree + " Derece " + fahranhayt + " fahranhayt")
+
+// Örnek-3
+// y=3x+4k ==>1.dereceden 2 bilinmeyenli denklem algoritması
+// Kullanıcı tarafından alınan x ve kdeğerlerini hesaplayan algoritma yazınız ?
+
+// örnek-4
+// operatör işlemleri: aşağıdaki örneği javascript ile yapalım
+// 4+3*2(3:3-1*6+9:1+(3:3));
+
+
+// cast
+// undefined NaN isNaN infinity
+// Math
+// Sayı
+// Kelime
+// String
+// Fonkisyonlar
